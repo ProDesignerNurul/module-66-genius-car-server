a60d170e7441a46fae59b7e1780e8afc7aa3148f
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-01 22:24:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zwnyjff.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('server is running on port 5000');
+})
+
+app.listen(port, () => {
+    console.log(`kaj choltese port ${port} a`)
+})
